a07efdea62d67d9254d3c3c367142fa0f1b85487
Dazcam <camerond@cardiff.ac.uk>
2025-06-03 16:23:31 +0100

    Add script to get sig eQTL for sldsr

---
+++ workflow/scripts/get_sig_egenes_with_cis_window.R
@@ -0,0 +1,61 @@
+#--------------------------------------------------------------------------------------
+#
+#    Extract sig. eGenes with cis window
+#
+#--------------------------------------------------------------------------------------
+
+## Load libraries and variables -------------------------------------------------------
+library(tidyverse)
+library(biomaRt)
+
+if (exists("snakemake")) {
+  log_smk <- function() {
+    if (exists("snakemake") & length(snakemake@log) != 0) {
+      log <- file(snakemake@log[1][[1]], open = "wt")
+      sink(log, append = TRUE)
+      sink(log, append = TRUE, type = "message")
+    }
+  }
+log_smk()
+message('Extracting table of sig. eGenes with cis-window ...')
+eqtl_file <- snakemake@input
+out_file <- snakemake@output
+
+# Load eQTL results from tensorQTL output
+message("Loading eQTL file ...")
+eqtl_data <- read_tsv(eqtl_file)
+
+# Extract unique genes with q-value < 0.05
+message("Extract genes @ FDR < 0.05 ...")
+sig_genes <- eqtl_data %>%
+  filter(qval < 0.05) %>%
+  distinct(phenotype_id) %>%
+  pull(phenotype_id)
+
+# Connect to Ensembl (hg38) using biomaRt
+message("Use biomaRt to get gene window cordinates on hg38 ...")
+mart <- useMart("ensembl", dataset = "hsapiens_gene_ensembl")
+
+# Fetch TSS information for significant genes
+tss_info <- getBM(
+  attributes = c("ensembl_gene_id", "chromosome_name", "start_position", "end_position", "strand"),
+  filters = "ensembl_gene_id",
+  values = sig_genes,
+  mart = mart
+) %>%
+  as_tibble() %>%
+  mutate(
+    tss = if_else(strand == 1, start_position, end_position),
+    cis_start = pmax(0, tss - 1e6),
+    cis_end = tss + 1e6
+  )
+
+# Save the annotated cis-windows
+message("Writing tsv file ...")
+tss_info %>%
+  select(ensembl_gene_id, chromosome_name, cis_start, cis_end) %>%
+  write_tsv(out_file)
+
+message("All done.")
+#--------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------
