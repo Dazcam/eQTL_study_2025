8467db079a3c74d27503e2362c9c011ec69451a1
Dazcam <camerond@cardiff.ac.uk>
2025-06-17 10:57:57 +0100

    Update ldsr rules; working up until ldscore generation

--- workflow/scripts/get_ldsr_hg19_refs.sh
+++ workflow/scripts/get_ldsr_hg19_refs.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-# Run from workflow dir 
+# Run from workflow dir
 
 # Set variables
 LDSR_ROOT=../resources/ldsr/
-LDSR_DIR=../resources/ldsr/reference_files
+LDSR_DIR=../resources/ldsr/ldsr_hg19_refs
 WEB_ROOT="https://zenodo.org/records/10515792/files/"
 
 # Clone repo
-git clone git@github.com:bulik/ldsc.git ${LDSR_ROOT}
+#git clone git@github.com:bulik/ldsc.git ${LDSR_ROOT}
 
 # Create dir for LDSR support files
 mkdir -p ${LDSR_DIR}
@@ -15,30 +15,58 @@ mkdir -p ${LDSR_DIR}
 # Declare associative array for expected extraction names
 declare -A extract_dirs=(
     ["1000G_Phase3_baseline_v1.2_ldscores"]="baseline_v1.2"
-    ["1000G_Phase3_frq"]="1000G_Phase3_frq"
-    ["1000G_Phase3_ldscores"]="LDscore"
-    ["1000G_Phase3_plinkfiles"]="1000G_EUR_Phase3_plink"
-    ["1000G_Phase3_weights_hm3_no_MHC"]="1000G_Phase3_weights_hm3_no_MHC"
+    ["1000G_Phase3_ldscores"]="ldscores"
+    ["1000G_Phase3_frq"]="frq"
+    ["1000G_Phase3_plinkfiles"]="plink_files"
+    ["1000G_Phase3_weights_hm3_no_MHC"]="weights"
 )
 
 # Download and extract files in a loop
 for file in "${!extract_dirs[@]}"; do
     tgz_file="${file}.tgz"
-    wget "${WEB_ROOT}${tgz_file}?download=1" -O "${LDSR_DIR}/${tgz_file}"
-  
-    # Extract into LDSR_DIR
-    tar -zxf "${LDSR_DIR}/${tgz_file}" -C "${LDSR_DIR}"
-    extracted_path="${LDSR_DIR}/${extract_dirs[$file]}"
-    target_path="${LDSR_DIR}/${file}"
+    url="${WEB_ROOT}${tgz_file}?download=1"
+    output_path="${LDSR_DIR}/${tgz_file}"
 
-    # Check if extracted folder is different from the target path before moving
+    echo "Downloading ${url} -> ${output_path}"
+    wget "${url}" -O "${output_path}"
+
+    # Extract into LDSR_DIR
+    echo "Extracting ${output_path} to ${LDSR_DIR}"
+    tar -zxf "${output_path}" -C "${LDSR_DIR}"
+
+    # Set extracted_path, with a fallback for the plinkfiles case
+    extracted_path="${LDSR_DIR}/${file}"
+
+    if [[ ! -d "$extracted_path" ]]; then
+        # Special case: for plinkfiles, folder is named without 'files'
+        if [[ "$file" == "1000G_Phase3_plinkfiles" ]]; then
+            extracted_path="${LDSR_DIR}/1000G_EUR_Phase3_plink"
+        fi
+    fi
+
+    target_path="${LDSR_DIR}/${extract_dirs[$file]}"
+
+    # Rename extracted folder to target name
     if [[ "$extracted_path" != "$target_path" ]]; then
+        echo "Moving ${extracted_path} -> ${target_path}"
         mv "$extracted_path" "$target_path"
     fi
-    
-    # Remove the downloaded .tgz fil
-    rm -rf "${LDSR_DIR}/${tgz_file}"
+
+    echo "Removing ${output_path}"
+    rm -rf "${output_path}"
 done
 
+# Move frq files into plink dir
+echo "Moving frequency files into plink directory"
+mv "${LDSR_DIR}/frq/"* "${LDSR_DIR}/plink_files/"
+
 # Download hm3_no_MHC.list.txt separately
-wget "${WEB_ROOT}hm3_no_MHC.list.txt?download=1" -O "${LDSR_DIR}/hm3_no_MHC.list.txt"
+hm3_url="${WEB_ROOT}hm3_no_MHC.list.txt?download=1"
+hm3_out="${LDSR_DIR}/hm3_no_MHC.list.txt"
+echo "Downloading ${hm3_url} -> ${hm3_out}"
+wget "${hm3_url}" -O "${hm3_out}"
+
+#Tidy
+rm -rf "${LDSR_DIR}/frq/"
+rm -rf "${LDSR_DIR}/baseline_v1.2/1000G_Phase3_baseline_v1.2_ldscores.tgz"
+rm -rf "${LDSR_DIR}/weights/1000G_Phase3_weights_hm3_no_MHC"
