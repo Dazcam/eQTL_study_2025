4b82a26e0389d12b13b0dbdf854f5da2de732d84
Dazcam <camerond@cardiff.ac.uk>
2025-07-24 13:22:04 +0100

    Add logging and warnings for SNP mismatches / removal

--- workflow/scripts/prep_gwas_for_smr.R
+++ workflow/scripts/prep_gwas_for_smr.R
@@ -4,74 +4,115 @@
 #
 #--------------------------------------------------------------------------------------
 
-## Set up logging for smk  ------------------------------------------------------------
+## Set up logging for Snakemake
 if (exists("snakemake")) {
   log_smk <- function() {
-    if (exists("snakemake") & length(snakemake@log) != 0) {
-      log <- file(snakemake@log[1][[1]], open = "wt")
+    if (exists("snakemake") && length(snakemake@log) != 0) {
+      log <- file(snakemake@log[[1]], open = "wt")
       sink(log, append = TRUE)
       sink(log, append = TRUE, type = "message")
     }
   }
+  log_smk()
 }
-log_smk()
-message('\n\nMunge GWAS for SMR input ...')
+
+message("\n\nMunge GWAS for SMR input ...")
 
 # Load packages
 library(tidyverse)
+library(knitr)
 
 # Input and output paths
-gwas_in <- snakemake@input[['gwas']]
-frq_in <- snakemake@input[['frq']]
-gwas_out <- snakemake@output
+gwas_in <- snakemake@input[["gwas"]]
+frq_in <- snakemake@input[["frq"]]
+gwas_out <- snakemake@output[[1]]
 
 # Read in data
-message('\nLoading data ...\n')
-gwas_tbl <- read_tsv(gwas_in) |>
-  rename(b = BETA, se = SE, p = PVAL) |>
+message("\nLoading data ...\n")
+gwas_tbl <- read_tsv(gwas_in, 
+                     col_types = cols(.default = "c", BP = "i", PVAL = "d", 
+                                      BETA = "d", SE = "d", Z = "d", N = "i")) %>%
+  rename(b = BETA, se = SE, p = PVAL) %>%
   select(SNP, CHR, BP, A1, A2, b, se, p, N)
 
-frq_tbl <- read_table(frq_in)
+frq_tbl <- read_tsv(frq_in, col_types = cols(.default = "c", MAF = "d", NCHROBS = "i"))
 
 # Join GWAS with reference frequencies by CHR and SNP
-message('Appending freq infor to GWAS ...')
+message("Appending freq info to GWAS ...\n")
 mrg_tbl <- gwas_tbl %>%
   left_join(frq_tbl, by = c("CHR", "SNP"), suffix = c("", ".ref"))
 
-# Assign freq based on allele alignment
-message('Checking for allele mismatches between freq ref to GWAS ...\n')
+# Check initial mismatches to determine strand flip handling
+initial_mismatch_tbl <- mrg_tbl %>%
+  filter(!(A1 == A1.ref & A2 == A2.ref | A1 == A2.ref & A2 == A1.ref) & !is.na(A1.ref))
+use_strand_flip <- nrow(initial_mismatch_tbl) > 100
+message(str_glue("Initial mismatches: {nrow(initial_mismatch_tbl)}. Strand flip handling: {ifelse(use_strand_flip, 'enabled', 'disabled')}.\n"))
+
+# Assign freq and status based on allele alignment
+message("Assigning frequencies and checking for allele mismatches ...\n")
 mrg_tbl <- mrg_tbl %>%
   mutate(
     freq = case_when(
-      A1 == A1.ref & A2 == A2.ref ~ MAF,
-      A1 == A2.ref & A2 == A1.ref ~ 1 - MAF,
-      TRUE ~ NA_real_
+      A1 == A1.ref & A2 == A2.ref ~ MAF,           # Alleles align directly
+      A1 == A2.ref & A2 == A1.ref ~ 1 - MAF,       # Alleles are flipped
+      # Strand flips for A/T or C/G SNPs (only if mismatches > 100)
+      use_strand_flip & (
+        (A1 == "A" & A2 == "T" & A1.ref == "T" & A2.ref == "A") |
+          (A1 == "T" & A2 == "A" & A1.ref == "A" & A2.ref == "T") |
+          (A1 == "C" & A2 == "G" & A1.ref == "G" & A2.ref == "C") |
+          (A1 == "G" & A2 == "C" & A1.ref == "C" & A2.ref == "G")
+      ) ~ 1 - MAF,                                # Treat as flipped
+      TRUE ~ NA_real_                              # Mismatch or missing
+    ),
+    allele_status = case_when(
+      A1 == A1.ref & A2 == A2.ref ~ "aligned",
+      A1 == A2.ref & A2 == A1.ref ~ "flipped",
+      use_strand_flip & (
+        (A1 == "A" & A2 == "T" & A1.ref == "T" & A2.ref == "A") |
+          (A1 == "T" & A2 == "A" & A1.ref == "A" & A2.ref == "T") |
+          (A1 == "C" & A2 == "G" & A1.ref == "G" & A2.ref == "C") |
+          (A1 == "G" & A2 == "C" & A1.ref == "C" & A2.ref == "G")
+      ) ~ "strand_flipped",
+      is.na(A1.ref) ~ "missing_reference",
+      TRUE ~ "mismatched"
     )
   )
-glimpse(mrg_tbl)
 
-# Check for mismatches or missing frequencies
-message('Mismatch tbl: \n')
+# Log allele alignment statistics
+allele_counts <- mrg_tbl %>%
+  count(allele_status) %>%
+  mutate(percentage = n / sum(n) * 100)
+message("Allele alignment summary:\n")
+walk2(allele_counts$allele_status, allele_counts$percentage, 
+      ~ message(str_glue("{.x}: {.y}% (n = {allele_counts$n[allele_counts$allele_status == .x]})")))
+
+# Check for true allele mismatches (excluding missing reference)
+message("Checking for true allele mismatches ...\n")
 mismatch_tbl <- mrg_tbl %>%
   filter(is.na(freq) & !is.na(A1.ref))
-glimpse(mismatch_tblmrg_tbl)
-
 if (nrow(mismatch_tbl) > 0) {
-  warning(str_glue("\n{nrow(mismatches)} SNPs have mismatched alleles or missing frequencies and will be excluded"))
+  warning(str_glue("\n{nrow(mismatch_tbl)} SNPs have mismatched alleles (e.g., non-strand-flip incompatibilities) and will be excluded"))
+}
+
+# Check for missing reference SNPs
+missing_ref_tbl <- mrg_tbl %>%
+  filter(is.na(freq) & is.na(A1.ref))
+if (nrow(missing_ref_tbl) > 0) {
+  warning(str_glue("\n{nrow(missing_ref_tbl)} SNPs are missing from the reference and will be excluded"))
 }
 
 # Filter out SNPs with missing freq
-message('Filtering mismatches ...\n')
+message("Filtering SNPs with missing or mismatched frequencies ...\n")
 result <- mrg_tbl %>%
   filter(!is.na(freq)) %>%
   select(SNP, A1, A2, freq, b, se, p, N)
 
 # Write output
-message('Writing GWAS in .ma format ...')
-write_tsv(result, output_file, na = "NA")
+message(str_glue("Writing GWAS in .ma format to {gwas_out} ...\n"))
+write_tsv(result, gwas_out, na = "NA")
 
-message(str_glue("Formatted GWAS data written to {output_file}"))
-message('Done.')
+message(str_glue("Formatted GWAS data written to {gwas_out}"))
+message("Done.\n")
 
 #--------------------------------------------------------------------------------------
 #--------------------------------------------------------------------------------------
