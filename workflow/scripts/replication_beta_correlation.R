eb332fe999e357833655b74b1dc84cd55e86401f
Dazcam <camerond@cardiff.ac.uk>
2026-01-23 09:47:26 +0000

    Add expPC mappings to beta corr script

--- workflow/scripts/replication_beta_correlation.R
+++ workflow/scripts/replication_beta_correlation.R
@@ -24,6 +24,10 @@ message("\n\nGenerating data for eQTL beta correlation analysis ...")
 # -------------------------------------------------------------------------------------
 
 out_file <- snakemake@output[[1]]
+in_dir <- snakemake@params[['in_dir']] 
+fugita_dir <- snakemake@params[['fugita_dir']] 
+#'../resources/public_datasets/fugita_2024/'
+#'../results/05TENSORQTL/tensorqtl_perm/'
 
 # Load required libraries
 library(tidyverse)
@@ -31,21 +35,45 @@ library(data.table)
 
 # Define variables
 cell_types <- c(
-  "ExN-UL", "ExN-UL-0", "ExN-UL-1", "ExN-UL-2",
-  "ExN-DL", "ExN-DL-0", "ExN-DL-1", "ExN-DL-2", "ExN-DL-3",
-  "RG", "RG-0", "RG-1", "RG-2", "RG-3",
-  "InN", "InN-0", "InN-1",
+  "Glu-UL", "Glu-UL-0", "Glu-UL-1", "Glu-UL-2",
+  "Glu-DL", "Glu-DL-0", "Glu-DL-1", "Glu-DL-2", 
+  "NPC", "NPC-0", "NPC-1", "NPC-2", 
+  "GABA", "GABA-0", "GABA-1", "GABA-2",
   "Endo-Peri",
   "OPC",
   "MG"
 )
 
+# Exp PC map
+expPC_map_all <- c(
+  "Glu-UL"     = 50,
+  "Glu-DL"     = 40,
+  "GABA"       = 30,
+  "NPC"        = 30,
+  "MG"         = 30,
+  "OPC"        = 30,
+  "Endo-Peri"  = 30,
+  "Glu-UL-0"     = 30,
+  "Glu-UL-1"     = 50,
+  "Glu-UL-2"     = 40,
+  "Glu-DL-0"     = 40,
+  "Glu-DL-1"     = 40,
+  "Glu-DL-2"     = 30,
+  "GABA-0"       = 30,
+  "GABA-1"       = 40,
+  "GABA-2"       = 40,
+  "NPC-0"        = 50,
+  "NPC-1"        = 30,
+  "NPC-2"        = 20
+)
+
+
 exp_PCs <- 40
 gen_PCs <- 4
 norm_method <- 'quantile' 
-in_dir <- '../results/05TENSORQTL/tensorqtl_perm/'
 
-fugita_dir <- '../resources/public_datasets/fugita_2024/'
+
+
 fugita_cell_types <- c('Ast', 'Exc', 'Inh', 'Oli', 'OPC', 'Mic', 'End')
 
 # Step 1: Pool unique sig. SNP-gene pairs from eQTL data, max(abs(beta)) for dups
@@ -53,6 +81,9 @@ my_data_list <- list()
 
 message('Loading sig.eQTL data ...\n')
 for (cell_type in cell_types) {
+  
+  expPC <- expPC_map_all[[ct]]
+  
   log_dir <- paste0(in_dir, cell_type, '_', norm_method, '_genPC_', gen_PCs, '_expPC_', exp_PCs, '/')
   file_path <- paste0(log_dir, cell_type, '_', norm_method, '_perm.cis_qtl.txt.gz')
   
@@ -143,14 +174,14 @@ rm(all_fugita_df, pooled_fugita_dt)
 gc() 
 
 
-message('Fugita SNP-gene pairs after dup rm: ', nrow(pooled_fugita))
+message('\nFugita SNP-gene pairs after dup rm: ', nrow(pooled_fugita))
 message('Any NAs in pooled Fugita data? ', anyNA(pooled_fugita))
-message('Pooled Fugita tbl:\n')
+message('\nPooled Fugita tbl:\n')
 pooled_fugita
 
 # Join with your pooled data for paired betas
 paired_betas <- pooled_my %>%
-  inner_join(pooled_fugita, by = c('key')) %>%
+  GABAer_join(pooled_fugita, by = c('key')) %>%
   select(key, beta_my, beta_fugita)
 message('Number of overlapping SNP-gene pairs: ', nrow(paired_betas))
 message('Paired betas tbl:\n')
@@ -161,34 +192,3 @@ write_tsv(paired_betas, out_file)
 
 #--------------------------------------------------------------------------------------
 #--------------------------------------------------------------------------------------
-
-# # Run correlation analysis (Pearson)
-# cor_result <- cor.test(paired_betas$beta_my, paired_betas$beta_fugita, method = 'pearson')
-# print(cor_result) 
-# 
-# cor_r <- round(cor_result$estimate, 3)
-# cor_p <- ifelse(cor_result$p.value < 1e-3, "< 0.001", round(cor_result$p.value, 3))
-# cor_label <- paste0("r = ", cor_r, ", p = ", cor_p)
-# 
-# # Generate publication-ready plot
-# p <- ggplot(paired_betas, aes(x = beta_fugita, y = beta_my)) +
-#   geom_point(alpha = 0.6, color = 'steelblue', size = 1.5) +
-#   geom_smooth(method = 'lm', color = 'red', se = TRUE, linewidth = 1) +
-#   annotate("text", x = Inf, y = Inf, label = cor_label, hjust = 1.02, vjust = 1.02,
-#            size = 4, fontface = "bold") +
-#   labs(
-#     x = 'Beta (Query)',
-#     y = 'Beta (Reference)'
-#   ) +
-#   theme_minimal() +  # Publication-ready theme from ggpubr
-#   theme(
-#     plot.title = element_text(hjust = 0.5, face = 'bold'),
-#     axis.title = element_text(face = 'bold')
-#   )
-# 
-# # Display plot
-# print(p)
-# 
-# # Save plot to file (e.g., for publication)
-# ggsave('eqtl_beta_correlation_plot.png', p, width = 8, height = 6, dpi = 300, bg = 'white')
-# ggsave('eqtl_beta_correlation_plot.pdf', p, width = 8, height = 6)  
